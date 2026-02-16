f896dfa07bea656d250a0f4d89a6ec850c7788a2
CodingStickbug <stefangrblr7@gmail.com>
2026-02-16 09:20:07 +0200

    Added all pages

--- SSDCourse/ecommerce-project/src/App.jsx
+++ SSDCourse/ecommerce-project/src/App.jsx
@@ -2,12 +2,16 @@ import { HomePage } from "./Pages/HomePage";
 import { Route, Routes } from "react-router";
 import "./App.css";
 import { CheckoutPage } from "./Pages/CheckoutPage";
+import { OrdersPage } from "./Pages/OrdersPage";
+import { TrackingPage } from "./Pages/TrackingPage";
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/orders" element={<OrdersPage />} />
+      <Route path="/tracking" element={<TrackingPage />} />
     </Routes>
   );
 }
